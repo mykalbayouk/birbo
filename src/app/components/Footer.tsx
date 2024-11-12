5ceb86a8217d1bc7c9c55779166daf6ee8e4bf18
Lera <vl47623@uga.edu>
2024-11-12 16:17:31 -0500

    made footer and put footer in page.tsx

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full flex justify-center items-center bg-[#fdf7e1] p-4 rounded shadow-md fixed bottom-0">
+      <p className="font-bold text-gray-800">Group Awesome</p>
+    </footer>
+  )
+}
