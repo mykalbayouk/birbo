3719c905669a83d4ce28679a6e3bdc33d0474062
amanilester <118773252+amanilester@users.noreply.github.com>
2024-11-19 15:39:13 -0500

    Signup routing created, updated userSchema.ts to require unique email/username

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,23 @@
+import connectMongoDB from "@/libs/mongodb";
+import { User } from "@/models/userSchema";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: NextRequest) {
+    await connectMongoDB();
+    const { username, email, password } = await request.json();
+
+    const hashedPassword = await bcrypt.hash(password, 5);
+    const newUser = {
+        username,
+        password: hashedPassword,
+        email
+    }
+    try {
+        await User.create(newUser);
+        return NextResponse.json({ message: "User created" }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Some error occurred" }, { status: 500 });
+    }
+}
