a60cee5f55040077ea3bef0962e27888f8828ac9
Michael Bayouk <michaelbayouk@gmail.com>
2024-11-19 14:32:54 -0500

    added title to create form

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -6,13 +6,14 @@ import { useState } from "react";
 export default function Create() {
     const router = useRouter();
 
+    const [title, setTitle] = useState("");
     const [imgLink, setImgLink] = useState("");
     const [caption, setCaption] = useState("");
 
     const handleCreate = (e: React.FormEvent) => {
         e.preventDefault();
-        const formData = { imgLink, caption };
-        if (imgLink === "" || caption === "") {
+        const formData = { title, imgLink, caption };
+        if (title === "" || imgLink === "" || caption === "") {
             alert("Please fill out all fields");
             return;
         }
@@ -29,6 +30,11 @@ export default function Create() {
             <div className="bg-[#fdf7e1] p-12 rounded shadow-md max-w-md w-full">
                 <h2 className="text-xl font-semibold mb-4 text-center text-gray-900">Create Post</h2>
                 <form onSubmit={handleCreate} className="flex flex-col space-y-4">
+                    <label htmlFor="title" className="block mb-2 text-gray-800 text-center">
+                        Insert Title:
+                        <input onChange={(e) => setTitle(e.target.value)}
+                            type="text" className="w-full mt-1 p-2 rounded bg-gray-200" />
+                    </label>
                     <label htmlFor="imgLink" className="block mb-2 text-gray-800 text-center">
                         Insert Link to Image:
                         <input onChange={(e) => setImgLink(e.target.value)}
@@ -36,8 +42,8 @@ export default function Create() {
                     </label>
                     <label htmlFor="caption" className="block mb-4 text-gray-800 text-center">
                         Insert Caption:
-                        <input onChange={(e) => setCaption(e.target.value)}
-                            type="text" className="w-full mt-1 p-2 rounded bg-gray-200" />
+                        <textarea onChange={(e) => setCaption(e.target.value)}
+                            className="w-full mt-1 p-2 rounded bg-gray-200 h-24" />
                     </label>
                     <button type="submit" className="w-full bg-[#fdd28e] py-2 rounded mt-4 text-gray-900">Create Post</button>
                 </form>
