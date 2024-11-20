76307478d6c9c9d59e36170694f687d66dde9f66
Michael Bayouk <michaelbayouk@gmail.com>
2024-11-20 01:22:02 -0500

    message

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 
-export default function PostCard({ post }: { post: { id: number; img: string; caption: string } }) {
+export default function PostCard({ post }: { post: { id: number; image: string; description: string, title: string, user: string } }) {
+  console.log("PostCard:", post);
   return (
-    <div className="bg-[#fdf7e1] rounded-lg shadow-md p-4 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
-      
-      <img src={post.img} alt={post.caption} className="bg-gray-300 h-32 rounded mb-2" />
+    <div className="bg-[#fdf7e1] rounded-lg shadow-md p-4 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl max-h-56 overflow-hidden">
+    <div className="flex flex-col items-center">
+      <img src={post.image} alt={post.title} className="bg-gray-300 h-32 rounded mb-2" />
       <div className="text-center text-gray-800">
-        <p>{post.caption}</p>
-        <p className="text-gray-700">Posted by &lt;name&gt;</p>
+      <p>{post.description}</p>
+      <p className="text-gray-500 ">Posted by {post.user}</p>
       </div>
+    </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,42 +4,68 @@ import Navbar from "./components/Navbar";
 import PostCard from "./components/PostCard";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
 
-interface Post {
+export interface Post {
   id: number;
-  img: string;
-  caption: string;
+  title: string;
+  user: string;
+  image: string;
+  description: string;
 }
 
-const posts: Post[] = [
-  { id: 1, img: "https://images.unsplash.com/photo-1486365227551-f3f90034a57c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmlyZHxlbnwwfHwwfHx8MA%3D%3D", caption: "A cute bird I spotted" },
-  { id: 2, img: "https://t3.ftcdn.net/jpg/06/10/68/10/360_F_610681083_M6XlAUkKj0I9ykA0Iz1ysOTCsNvpU5Vw.jpg", caption: "So colorful!" },
-  { id: 3, img: "https://www.allaboutbirds.org/news/wp-content/uploads/2024/09/TOC-Autumn24-Ruby-crowned_Kinglet-Christopher_T-ML609692481-FI-480x360.jpg", caption: "Such a funny guy." },
-  { id: 4, img: "https://t4.ftcdn.net/jpg/05/65/36/03/360_F_565360370_LrWWCTxczrmwqpsPYPljiFyE4gFqpecr.jpg", caption: "He is so adorable!!!" },
-];
+const fetchPosts = async () => {
+  const response = await fetch("/api/items", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response;
+  }).then((response) => response)
+  .catch((error) => {
+    console.error("Error:", error);
+    throw error;
+  });
+
+  const data = await response.json();
+  return data;
+};
 
 export default function Home() {
-  let isLogged: boolean = false;
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLogged, setIsLogged] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const fetchedPosts = await fetchPosts();
+      const arr = Object.keys(fetchedPosts).map((key) => fetchedPosts[key]).at(0);
+      setPosts(arr);
+    };
+    
+    getPosts();
+  }, []);
 
   return (
     <div className="min-h-screen bg-[#f4d9a0] flex flex-col items-center p-4">
-      <Navbar logged={isLogged}/>
+      <Navbar logged={isLogged} />
 
       <main className="flex flex-wrap w-full max-w-screen-lg gap-6 mt-8">
-
         <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 flex-1">
           {posts.map((po, index) => (
-            <PostCard key={index} post={po}/>
+            <PostCard key={index} post={po} />
           ))}
         </section>
-
 
         <aside className="w-full sm:w-1/3 lg:w-1/4">
           <Sidebar logged={isLogged} />
         </aside>
       </main>
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
